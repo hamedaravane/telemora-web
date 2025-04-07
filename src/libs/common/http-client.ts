3c779806644063ac7f9f851e0a3cb7cb829e30fa
Hamed Arghavan <hamedaravane@gmail.com>
2025-04-07 12:27:55 +0330

    refactor(http-client): remove unnecessary undefined type from request config
    - Remove | undefined from AxiosRequestConfig type in HTTP methods
    - Simplify type definitions for request configurations

--- src/libs/common/http-client.ts
+++ src/libs/common/http-client.ts
@@ -2,15 +2,15 @@ import axios from './axios';
 import { AxiosRequestConfig } from 'axios';
 
 export const httpClient = {
-  get: <T>(url: string, config?: AxiosRequestConfig<unknown> | undefined) =>
+  get: <T>(url: string, config?: AxiosRequestConfig<unknown>) =>
     axios.get<T>(url, config).then((res) => res.data),
-  post: <T>(url: string, data?: unknown, config?: AxiosRequestConfig<unknown> | undefined) =>
+  post: <T>(url: string, data?: unknown, config?: AxiosRequestConfig<unknown>) =>
     axios.post<T>(url, data, config).then((res) => res.data),
-  patch: <T>(url: string, data?: unknown, config?: AxiosRequestConfig<unknown> | undefined) =>
+  patch: <T>(url: string, data?: unknown, config?: AxiosRequestConfig<unknown>) =>
     axios.patch<T>(url, data, config).then((res) => res.data),
-  put: <T>(url: string, data?: unknown, config?: AxiosRequestConfig<unknown> | undefined) =>
+  put: <T>(url: string, data?: unknown, config?: AxiosRequestConfig<unknown>) =>
     axios.put<T>(url, data, config).then((res) => res.data),
-  delete: <T>(url: string, config?: AxiosRequestConfig<unknown> | undefined) =>
+  delete: <T>(url: string, config?: AxiosRequestConfig<unknown>) =>
     axios.delete<T>(url, config).then((res) => res.data),
 };
 
