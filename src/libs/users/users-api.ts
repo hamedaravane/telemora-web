b96fe715c01905603e57e2f44aaad86437b9848a
Hamed Arghavan <hamedaravane@gmail.com>
2025-04-08 17:55:25 +0330

    feat(users): implement new API functions for user profile updates
    - Add type imports for UpdateContactLocationDto and UpdateProfileDto
    - Implement updateProfile function for updating user profiles
    - Implement updateLanguage function for updating user language preferences
    - Implement updateContactLocation function for updating user contact locations
    - Refactor sendTelegramInitData function to use generic type

--- src/libs/users/users-api.ts
+++ src/libs/users/users-api.ts
@@ -1,6 +1,21 @@
-import { UserPrivateProfile } from '@/libs/users/types';
+import { UpdateContactLocationDto, UpdateProfileDto, UserPrivateProfile } from '@/libs/users/types';
 import httpClient from '@/libs/common/http-client';
 
-export async function sendTelegramInitData(initData: string): Promise<UserPrivateProfile> {
-  return httpClient.post(`/users/init-data`, { initData });
+export async function sendTelegramInitData(initData: string) {
+  return httpClient.post<UserPrivateProfile>(`/users/init-data`, { initData });
+}
+
+export async function updateProfile(telegramId: number | string, data: UpdateProfileDto) {
+  return httpClient.patch<UserPrivateProfile>(`/users/profile/${telegramId}`, { data });
+}
+
+export async function updateLanguage(telegramId: number | string, languageCode: string) {
+  return httpClient.patch<UserPrivateProfile>(`/users/language/${telegramId}`, { languageCode });
+}
+
+export async function updateContactLocation(
+  telegramId: number | string,
+  data: UpdateContactLocationDto,
+) {
+  return httpClient.patch<UserPrivateProfile>(`/users/contact-location/${telegramId}`, { data });
 }
