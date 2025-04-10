796bf261568e0abc8d27f602ae09784e4339ca54
Hamed <hamedaravane@gmail.com>
2025-04-11 01:16:09 +0330

    refactor(components): update ErrorPage component with optional reload prop
    - Change the reload prop to an optional parameter in the ErrorPage function
    - Improve type safety and flexibility when using the ErrorPage component

--- src/components/shared/errorPage.tsx
+++ src/components/shared/errorPage.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { Button } from '@heroui/react';
 import { FaUndo } from 'react-icons/fa';
 
-export default function ErrorPage(reload: () => void) {
+export default function ErrorPage(reload?: () => void) {
   return (
     <div className="flex flex-col gap-y-4 min-h-screen justify-center items-center text-center">
       <Image src="/server-error.webp" alt="failed to load" priority width={160} height={160} />
