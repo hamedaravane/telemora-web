33ee517a4e97681037839ab4ed1bf60c619344ac
Hamed <hamedaravane@gmail.com>
2025-04-11 01:15:37 +0330

    refactor(components): update ErrorPage to include reload functionality
    - Add reload prop to ErrorPage component
    - Update Button to use reload function on press
    - Change button text from "Refresh" to "Reload"

--- src/components/shared/errorPage.tsx
+++ src/components/shared/errorPage.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 import { Button } from '@heroui/react';
 import { FaUndo } from 'react-icons/fa';
 
-export default function ErrorPage() {
+export default function ErrorPage(reload: () => void) {
   return (
     <div className="flex flex-col gap-y-4 min-h-screen justify-center items-center text-center">
       <Image src="/server-error.webp" alt="failed to load" priority width={160} height={160} />
       <h3>Something went wrong!</h3>
-      <Button variant="shadow">
+      <Button variant="shadow" onPress={reload}>
         <FaUndo />
-        Refresh
+        Reload
       </Button>
     </div>
   );
