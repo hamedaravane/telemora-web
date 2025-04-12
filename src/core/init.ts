e51e844ddf5ef1aeb1fc5827757e2c0f3ca53eb8
Hamed Arghavan <hamedaravane@gmail.com>
2025-04-12 17:42:45 +0330

    feat(core): initialize Telegram SDK and integrate eruda for debugging
    - Add eruda package for debugging
    - Implement init function to set up Telegram SDK
    - Mount necessary components for app functionality
    - Bind CSS variables for theme and viewport
    - Restore init data and handle viewport mounting
    - Optionally initialize eruda for debugging based on debug flag

---
+++ src/core/init.ts
@@ -0,0 +1,37 @@
+import {
+  init as initSDK,
+  initData,
+  miniApp,
+  mountBackButton,
+  mountMiniAppSync,
+  mountThemeParamsSync,
+  setDebug,
+  themeParams,
+  viewport,
+} from '@telegram-apps/sdk-react';
+
+export function init(debug: boolean): void {
+  setDebug(debug);
+
+  initSDK();
+
+  mountBackButton();
+  mountMiniAppSync();
+  mountThemeParamsSync();
+  initData.restore();
+  void viewport
+    .mount()
+    .then(() => {
+      viewport.bindCssVars();
+    })
+    .catch((e) => {
+      console.error('Something went wrong mounting the viewport', e);
+    });
+
+  miniApp.bindCssVars();
+  themeParams.bindCssVars();
+
+  if (debug) {
+    import('eruda').then((lib) => lib.default.init()).catch(console.error);
+  }
+}
