49e3e4bef9bda52b8f342aff527a9f74019e3123
Hamed Arghavan <hamedaravane@gmail.com>
2025-04-23 16:50:09 +0330

    Refactor init data retrieval in user context.
    Replaced `retrieveRawLaunchParams` with `retrieveRawInitData` for improved clarity and alignment with the SDK. Ensured `initData` is explicitly handled as non-null when storing in localStorage.

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -2,7 +2,7 @@
 
 import React, { createContext, type PropsWithChildren, useContext } from 'react';
 import { UserPrivateProfile } from '@/libs/users/types';
-import { retrieveRawLaunchParams } from '@telegram-apps/sdk-react';
+import { retrieveRawInitData } from '@telegram-apps/sdk-react';
 import { useTelegramAuth } from '@/libs/users/users-api';
 import ErrorPage from '@/components/shared/errorPage';
 import SplashScreen from '@/components/shared/splash-screen';
@@ -10,8 +10,8 @@ import SplashScreen from '@/components/shared/splash-screen';
 const UserContext = createContext<UserPrivateProfile | null>(null);
 
 export function UserProvider({ children }: PropsWithChildren) {
-  const initData = retrieveRawLaunchParams();
-  localStorage.setItem('telegram-init-data', initData);
+  const initData = retrieveRawInitData();
+  localStorage.setItem('telegram-init-data', initData!);
   const { data: user, isLoading, isError, refetch } = useTelegramAuth(initData);
 
   if (isLoading) {
