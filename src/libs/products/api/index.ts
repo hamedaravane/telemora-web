09842dc5545bcb00e9ed2ff935420e98337d95d1
Hamed Arghavan <hamedaravane@gmail.com>
2025-05-12 11:57:50 +0330

    Refactor API code for better readability and consistency.
    Removed unused imports and improved code formatting for better maintainability. Adjusted line breaks for ternary operator usage to enhance code clarity and readability. No functional changes were made to the API behavior.

--- src/libs/products/api/index.ts
+++ src/libs/products/api/index.ts
@@ -1,5 +1,3 @@
-import { faker } from '@faker-js/faker';
-
 import { isDev } from '@/libs/common/utils';
 import httpClient from '@/libs/common/utils/http-client';
 import {
@@ -7,14 +5,23 @@ import {
   generateMockProductPhotos,
   generateMockProductPreviews,
 } from '@/libs/products/mocks';
-import { CreateProductDto, ProductDetail, ProductPreview, UpdateProductDto } from '@/libs/products/types';
+import {
+  CreateProductDto,
+  ProductDetail,
+  ProductPreview,
+  UpdateProductDto,
+} from '@/libs/products/types';
 
 export async function getStoreProducts(storeId: number) {
-  return isDev ? generateMockProductPreviews() : httpClient.get<ProductPreview[]>(`/stores/${storeId}/products`);
+  return isDev
+    ? generateMockProductPreviews()
+    : httpClient.get<ProductPreview[]>(`/stores/${storeId}/products`);
 }
 
 export async function getProductDetails(storeId: number, productId: number) {
-  return isDev ? generateMockProductDetail() : httpClient.get<ProductDetail>(`/stores/${storeId}/products/${productId}`);
+  return isDev
+    ? generateMockProductDetail()
+    : httpClient.get<ProductDetail>(`/stores/${storeId}/products/${productId}`);
 }
 
 export async function uploadProductPhotos(storeId: number, productId: number, data: File) {
@@ -27,11 +34,15 @@ export async function uploadProductPhotos(storeId: number, productId: number, da
 }
 
 export async function createProduct(storeId: number, data: CreateProductDto) {
-  return isDev ? generateMockProductDetail() : httpClient.post<ProductDetail>(`/stores/${storeId}/products`, data);
+  return isDev
+    ? generateMockProductDetail()
+    : httpClient.post<ProductDetail>(`/stores/${storeId}/products`, data);
 }
 
 export async function updateProduct(storeId: number, productId: number, data: UpdateProductDto) {
-  return isDev ? generateMockProductDetail() : httpClient.patch<ProductDetail>(`/stores/${storeId}/products/${productId}`, data);
+  return isDev
+    ? generateMockProductDetail()
+    : httpClient.patch<ProductDetail>(`/stores/${storeId}/products/${productId}`, data);
 }
 
 export async function deleteProduct(storeId: number, productId: number) {
